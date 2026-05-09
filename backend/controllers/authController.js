8f0e7c658e8eaaf0e666caa7326e51e96797374a
ManineeKharsel <manineekharsel18@gmail.com>
2026-05-10 01:13:22 +0530

    fixed production cookie settings

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -101,8 +101,8 @@ export const loginUser = async (req, res) => {
     res
   .cookie("token", token, {
     httpOnly: true,     
-    secure: false,      
-    sameSite: "Lax",    
+    secure: true,      
+    sameSite: "None",    
     maxAge: 1 * 24 * 60 * 60 * 1000, 
   })
   .status(200)
